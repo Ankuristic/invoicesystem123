e3789ab7c36142b0d1a6265efa23f99d7502e0b6
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2021-08-06 13:08:25 +0530

    Add files via upload

---
+++ logger/logger.js
@@ -0,0 +1,43 @@
+// const { createLogger, transports, format } = require('winston');
+
+// const logger = createLogger({
+//   transports: [
+//     new transports.Console({
+//       level: 'info',
+//       format: format.combine(format.timestamp().format.json())
+//     })
+//   ]
+// });
+
+// module.exports = logger;
+
+const { format, createLogger, transports } = require('winston');
+const { timestamp, combine, errors, printf } = format;
+
+const logFormat = printf(({ level, timestamp, message, stack }) => {
+  return `${timestamp}  ${level}  ${message || stack}`;
+});
+
+const logger = createLogger({
+  format: combine(
+    timestamp(),
+    errors({ stack: true }),
+    logFormat,
+    format.colorize()
+  ),
+  transports: [
+    new transports.File({ filename: 'logs/complete.log' }),
+    new transports.File({ filename: 'logs/error.log', level: 'error' })
+  ]
+});
+
+const logHandler = {
+  log(level, message) {
+    logger.log({ level, message });
+  },
+  error(level, message) {
+    logger.error({ level, message });
+  }
+};
+
+module.exports = { logger: logHandler };
